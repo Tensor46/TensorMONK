b9a441e492610cd04d170d4e15de4cba316dd3b8
Tensor46 <vikas11187@gmail.com>
2018-06-14 17:32:52 -0500

    Support for unused nets

--- core/NeuralEssentials/LoadModel.py
+++ core/NeuralEssentials/LoadModel.py
@@ -13,7 +13,7 @@ def LoadModel(Model):
     dict_stuff = torch.load(file_name)
     what2load = [x for x in dir(Model) if x.startswith("net") or x.startswith("meter")]
     for x in what2load:
-        if "net" in x.lower():
+        if "net" in x.lower() and getattr(Model, x) is not None:
             eval("Model."+x+'.load_state_dict(dict_stuff["'+x+'"])')
         else:
             if dict_stuff[x] is not None: setattr(Model,x,dict_stuff[x])
