a7beb4dfb431d101f5dcc4c1b64538dc9c2f569d
Tensor46 <vikas11187@gmail.com>
2018-06-16 12:08:22 -0500

    maxout bug fix & added maxout(relu(tensor))

--- core/NeuralLayers/Convolution.py
+++ core/NeuralLayers/Convolution.py
@@ -4,6 +4,7 @@ __all__ = ["Convolution", ]
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 # ============================================================================ #
 
 
@@ -13,6 +14,17 @@ class MaxOut(nn.Module):
         super(MaxOut, self).__init__()
 
     def forward(self, tensor):
+        return torch.max(*tensor.split(tensor.size(1)//2, 1))
+# ============================================================================ #
+
+
+class ReluMaxOut(nn.Module):
+    """ maxout(relu(x)) """
+    def __init__(self):
+        super(ReluMaxOut, self).__init__()
+
+    def forward(self, tensor):
+        tensor = F.relu(tensor)
         return torch.max(*tensor.split(tensor.size(1)//2, 1))
 # ============================================================================ #
 
@@ -28,29 +40,31 @@ class Swish(nn.Module):
 # ============================================================================ #
 
 
-def ActivationFNs(activation, pre_nm):
+def ActivationFNs(activation):
     if activation == "relu":
-        return 1, 1, nn.ReLU()
+        return nn.ReLU()
     if activation == "relu6":
-        return 1, 1, nn.ReLU6()
+        return nn.ReLU6()
     if activation == "lklu":
-        return 1, 1, nn.LeakyReLU()
+        return nn.LeakyReLU()
     if activation == "tanh":
-        return 1, 1, nn.Tanh()
+        return nn.Tanh()
     if activation == "sigm":
-        return 1, 1, nn.Sigmoid()
+        return nn.Sigmoid()
     if activation == "maxo":
-        return (2, 1, MaxOut()) if pre_nm else (1, 2, MaxOut())
+        return MaxOut()
+    if activation == "rmxo":
+        return ReluMaxOut()
     if activation == "swish":
-        return 1, 1, Swish()
-    return 1, 1, None
+        return Swish()
+    return None
 # ============================================================================ #
 
 
 class Convolution(nn.Module):
     def __init__(self, tensor_size, filter_size, out_channels, strides=(1, 1),
                  pad=True, activation="relu", dropout=0., batch_nm=False,
-                 pre_nm=False, groups=1, weight_nm=False, *args, **kwargs):
+                 pre_nm=False, groups=1, weight_norm=False, *args, **kwargs):
         super(Convolution, self).__init__()
         # Checks
         assert len(tensor_size) == 4 and type(tensor_size) in [list, tuple], \
@@ -68,35 +82,34 @@ class Convolution(nn.Module):
             strides = tuple(strides)
         assert len(strides) == 2, "Convolution -- strides length must be 2"
         assert isinstance(pad, bool), "Convolution -- pad must be boolean"
-        assert isinstance(activation, str), "Convolution -- pad must be str"
         assert isinstance(dropout, float), "Convolution -- dropout must be float"
-        assert isinstance(batch_nm, bool), "Convolution -- batch_nm must be boolean"
-        assert isinstance(pre_nm, bool), "Convolution -- dropout must be boolean"
-        assert isinstance(groups, int), "Convolution -- dropout must be int"
-        assert isinstance(weight_nm, bool), "Convolution -- dropout must be boolean"
         activation = activation.lower()
         # Modules
         padding = (filter_size[0]//2, filter_size[1]//2) if pad else 0
         if dropout > 0.:
             self.dropout = nn.Dropout2d(dropout)
-        pre, pst = 1, 1
+        pre_expansion, pst_expansion = 1, 1
+        if pre_nm and activation in ("maxo", "rmxo"):
+            pre_expansion = 2
+        if not pre_nm and activation in ("maxo", "rmxo"):
+            pst_expansion = 2
         if pre_nm:
             if batch_nm:
                 self.Normalization = nn.BatchNorm2d(tensor_size[1])
-            pre, pst, act = ActivationFNs(activation, pre_nm)
+            act = ActivationFNs(activation)
             if act is not None:
                 self.Activation = act
-        if weight_nm:
+        if weight_norm:
             """ https://arxiv.org/pdf/1602.07868.pdf """
-            self.Convolution = nn.utils.weight_norm(nn.Conv2d(tensor_size[1]//pre, out_channels*pst, filter_size,
+            self.Convolution = nn.utils.weight_norm(nn.Conv2d(tensor_size[1]//pre_expansion, out_channels*pst_expansion, filter_size,
                                                               strides, padding, bias=False, groups=groups), name='weight')
         else:
-            self.Convolution = nn.Conv2d(tensor_size[1]//pre, out_channels*pst, filter_size, strides, padding, bias=False, groups=groups)
+            self.Convolution = nn.Conv2d(tensor_size[1]//pre_expansion, out_channels*pst_expansion, filter_size, strides, padding, bias=False, groups=groups)
             torch.nn.init.orthogonal_(self.Convolution.weight)
         if not pre_nm:
             if batch_nm:
-                self.Normalization = nn.BatchNorm2d(out_channels*pst)
-            pre, pst, act = ActivationFNs(activation, pre_nm)
+                self.Normalization = nn.BatchNorm2d(out_channels*pst_expansion)
+            act = ActivationFNs(activation)
             if act is not None:
                 self.Activation = act
         self.pre_nm = pre_nm
@@ -124,5 +137,5 @@ class Convolution(nn.Module):
 
 
 # x = torch.rand(3,3,10,10)
-# test = Convolution((1,3,10,10), (3,3), 16, (2,2), False, "maxo", 0.5, True, False)
+# test = Convolution((1,3,10,10), (3,3), 16, (2,2), False, "rmxo", 0., True, False)
 # test(x).size()
