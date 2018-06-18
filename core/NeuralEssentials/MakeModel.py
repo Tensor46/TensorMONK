61159c427b6ed4e20a9edd0a9ba814d048a86f63
Tensor46 <vikas11187@gmail.com>
2018-06-18 14:18:51 -0500

    + MakeAE

--- core/NeuralEssentials/MakeModel.py
+++ core/NeuralEssentials/MakeModel.py
@@ -32,9 +32,32 @@ def MakeCNN(file_name, tensor_size, n_labels,
             torch.cuda.set_device(default_gpu)
         Model.netEmbedding = Model.netEmbedding.cuda()
         Model.netLoss = Model.netLoss.cuda()
-    print(" --- Total parameters in netEmbedding :: {} ---".format(np.sum([np.prod(p.cpu().data.numpy().shape) \
+    print(" --- Total parameters in netEmbedding :: {} ---".format(np.sum([p.cpu().data.numel() \
           for p in Model.netEmbedding.parameters()])))
-    print(" --- Total parameters in netLoss      :: {} ---\n".format(np.sum([np.prod(p.cpu().data.numpy().shape) \
+    print(" --- Total parameters in netLoss      :: {} ---\n".format(np.sum([p.cpu().data.numel() \
           for p in Model.netLoss.parameters()])))
+    Model.is_cuda = is_cuda
+    return Model
+#==============================================================================#
+
+
+def MakeAE(file_name, tensor_size, n_labels,
+           autoencoder_net, autoencoder_net_kwargs,
+           default_gpu=0, gpus=1, ignore_trained=False):
+    Model = BaseModel()
+    Model.file_name = file_name
+    print("...... making PyTORCH model!")
+    autoencoder_net_kwargs["tensor_size"] = tensor_size
+    Model.netAE = CudaModel(is_cuda, gpus, autoencoder_net, autoencoder_net_kwargs)
+
+    if os.path.isfile(Model.file_name+("" if Model.file_name.endswith(".t7") else ".t7")) and not ignore_trained:
+        print("...... loading pretrained Model!")
+        Model = LoadModel(Model)
+    if is_cuda:
+        if gpus == 1:
+            torch.cuda.set_device(default_gpu)
+        Model.netAE = Model.netAE.cuda()
+    print(" --- Total parameters in netAE :: {} ---".format(np.sum([p.cpu().data.numel() \
+          for p in Model.netAE.parameters()])))
     Model.is_cuda = is_cuda
     return Model
