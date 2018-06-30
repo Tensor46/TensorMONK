983218301392b9d9d51b203ebddcf43e8db74214
Tensor46 <vikas11187@gmail.com>
2018-06-29 22:35:59 -0500

    + InceptionV4 model

--- core/NeuralArchitectures/__init__.py
+++ core/NeuralArchitectures/__init__.py
@@ -7,6 +7,7 @@ from .ShuffleNet import ShuffleNet
 from .MobileNetV1 import MobileNetV1
 from .MobileNetV2 import MobileNetV2
 from .ResidualNet import ResidualNet
+from .InceptionV4 import InceptionV4
 
 from .LinearVAE import LinearVAE
 from .ConvolutionalVAE import ConvolutionalVAE
