4910a9529760141d7301bd9e07abb7e5640c2bf8
Tensor46 <vikas11187@gmail.com>
2018-06-29 22:35:37 -0500

    + InceptionV4 modules

--- core/NeuralLayers/__init__.py
+++ core/NeuralLayers/__init__.py
@@ -6,7 +6,9 @@ from .ConvolutionTranspose import ConvolutionTranspose
 from .CarryResidue import ResidualOriginal, ResidualComplex, ResidualComplex2
 from .CarryResidue import ResidualInverted, ResidualShuffle
 from .CarryResidue import SimpleFire, CarryModular
+from .CarryResidue import Stem2, InceptionA, InceptionB, InceptionC, ReductionA, ReductionB
 from .PrimaryCapsule import PrimaryCapsule
 from .RoutingCapsule import RoutingCapsule
 
 from .LossFunctions import CapsuleLoss, CategoricalLoss
+from .ObfuscateDecolor import ObfuscateDecolor
