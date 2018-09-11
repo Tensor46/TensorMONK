8a4fedf66b2dafb631cb697ddcf7a6b185ee3cbf
Tensor46 <vikas11187@gmail.com>
2018-09-11 09:57:12 -0500

    bug-fix

--- core/NeuralArchitectures/densenet.py
+++ core/NeuralArchitectures/densenet.py
@@ -163,9 +163,9 @@ class DenseNet(nn.Module):
             if self.in_tensor_size[1] == 1: # convert to rgb
                 tensor = torch.cat((tensor, tensor, tensor), 1)
             if tensor.min() >= 0: # do imagenet normalization
-                tensor[:, 0].sub_(0.485).div_(0.229)
-                tensor[:, 1].sub_(0.456).div_(0.224)
-                tensor[:, 2].sub_(0.406).div_(0.225)
+                tensor[:, 0].add_(-0.485).div_(0.229)
+                tensor[:, 1].add_(-0.456).div_(0.224)
+                tensor[:, 2].add_(-0.406).div_(0.225)
 
         if hasattr(self, "embedding"):
             return self.embedding(self.network(tensor).view(tensor.size(0), -1))
