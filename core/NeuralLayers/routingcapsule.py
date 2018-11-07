23adbb178ce7a244ff5f2affb13dbcf672441d8c
Vikas Gottemukkula <tensor46@Vikass-MacBook-Pro.local>
2018-11-06 22:47:35 -0500

    - Variable import

--- core/NeuralLayers/routingcapsule.py
+++ core/NeuralLayers/routingcapsule.py
@@ -3,7 +3,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 #==============================================================================#
 
 
