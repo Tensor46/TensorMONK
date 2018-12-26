d5fc0fd1aaaf5a6e0096fb1c91cf1a71a1319a2d
VG <vikas11187@gmail.com>
2018-12-26 13:50:54 -0600

    Moved from Module to Sequential

--- core/NeuralArchitectures/mobilenetv1.py
+++ core/NeuralArchitectures/mobilenetv1.py
@@ -1,13 +1,10 @@
-""" TensorMONK's :: NeuralArchitectures                                      """
+""" TensorMONK's :: NeuralArchitectures                                     """
 
 import torch
-import torch.nn as nn
-import numpy as np
-from ..NeuralLayers import *
-#==============================================================================#
+from ..NeuralLayers import Convolution, Linear
 
 
-class MobileNetV1(nn.Module):
+class MobileNetV1(torch.nn.Sequential):
     """
         Implemented https://arxiv.org/pdf/1704.04861.pdf
 
@@ -15,54 +12,59 @@ class MobileNetV1(nn.Module):
         Works fairly well, for tensor_size of min(height, width) >= 128
     """
     def __init__(self,
-                 tensor_size = (6, 3, 224, 224),
-                 activation = "relu",
-                 normalization = "batch",
-                 pre_nm = False,
-                 weight_nm = False,
-                 equalized = False,
-                 embedding = False,
-                 shift = False,
-                 n_embedding = 256,
+                 tensor_size=(6, 3, 224, 224),
+                 activation: str = "relu",
+                 normalization: str = "batch",
+                 pre_nm: bool = False,
+                 weight_nm: bool = False,
+                 equalized: bool = False,
+                 shift: bool = False,
+                 n_embedding: int = None,
                  *args, **kwargs):
         super(MobileNetV1, self).__init__()
 
-        self.Net46 = nn.Sequential()
+        block_params = [(3, 32, 2, 1), (3, 32, 1, 32),
+                        (1, 64, 1, 1), (3, 64, 2, 64),
+                        (1, 128, 1, 1), (3, 128, 1, 128),
+                        (1, 128, 1, 1), (3, 128, 2, 128),
+                        (1, 256, 1, 1), (3, 256, 1, 256),
+                        (1, 256, 1, 1), (3, 256, 2, 256),
+                        (1, 512, 1, 1), (3, 512, 1, 512),
+                        (1, 512, 1, 1), (3, 512, 1, 512),
+                        (1, 512, 1, 1), (3, 512, 1, 512),
+                        (1, 512, 1, 1), (3, 512, 1, 512),
+                        (1, 512, 1, 1), (3, 512, 1, 512),
+                        (1, 512, 1, 1), (3, 512, 2, 512),
+                        (1, 1024, 1, 1), (3, 1024, 1, 1024), (1, 1024, 1, 1)]
 
-        block_params = [(3, 32, 2, 1), (3, 32, 1, 32), (1, 64, 1, 1), (3, 64, 2, 64),
-                        (1, 128, 1, 1), (3, 128, 1, 128), (1, 128, 1, 1), (3, 128, 2, 128),
-                        (1, 256, 1, 1), (3, 256, 1, 256), (1, 256, 1, 1), (3, 256, 2, 256),
-                        (1, 512, 1, 1), (3, 512, 1, 512), (1, 512, 1, 1), (3, 512, 1, 512),
-                        (1, 512, 1, 1), (3, 512, 1, 512), (1, 512, 1, 1), (3, 512, 1, 512),
-                        (1, 512, 1, 1), (3, 512, 1, 512), (1, 512, 1, 1), (3, 512, 2, 512),
-                        (1, 1024, 1, 1), (3, 1024, 1, 1024), (1, 1024, 1, 1), ]
+        kwargs = {"activation": activation, "normalization": normalization,
+                  "weight_nm": weight_nm, "equalized": equalized,
+                  "shift": shift}
 
         print("Input", tensor_size)
-        _tensor_size = tensor_size
+        t_size = tensor_size
         for i, (k, oc, s, g) in enumerate(block_params):
-            self.Net46.add_module("Mobile"+str(i), Convolution(_tensor_size, k,
-                oc, s, True, activation, 0., normalization, False if i == 0
-                else pre_nm, g, weight_nm, equalized, shift))
-            _tensor_size = self.Net46[-1].tensor_size
-            print("Mobile"+str(i), _tensor_size)
+            self.add_module("Mobile"+str(i),
+                            Convolution(t_size, k, oc, s, groups=g,
+                                        pre_nm=False if i == 0 else pre_nm,
+                                        **kwargs))
+            t_size = getattr(self, "Mobile"+str(i)).tensor_size
+            print("Mobile"+str(i), t_size)
 
-        self.Net46.add_module("AveragePool", nn.AvgPool2d(self.Net46[-1].tensor_size[2:]))
+        self.add_module("AveragePool", torch.nn.AvgPool2d(t_size[2:]))
         print("AveragePool", (1, 1024, 1, 1))
         self.tensor_size = (6, 1024)
 
-        if embedding:
-            self.embedding = nn.Linear(1024, n_embedding, bias=False)
+        if n_embedding is not None and n_embedding > 0:
+            self.add_module("Embedding", Linear(self.tensor_size, n_embedding,
+                                                "", 0., False))
             self.tensor_size = (6, n_embedding)
             print("Linear", (1, n_embedding))
 
-    def forward(self, tensor):
-        if hasattr(self, "embedding"):
-            return self.embedding(self.Net46(tensor).view(tensor.size(0), -1))
-        return self.Net46(tensor).view(tensor.size(0), -1)
 
-
-# from core.NeuralLayers import *
+# from core.NeuralLayers import Convolution, Linear
 # tensor_size = (1, 3, 224, 224)
 # tensor = torch.rand(*tensor_size)
-# test = MobileNetV1(tensor_size)
+# test = MobileNetV1(tensor_size, n_embedding=64)
 # test(tensor).size()
+# %timeit test(tensor).size()
