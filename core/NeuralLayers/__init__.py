1a582cdbe67040d9c49ca17848943d132e82dce3
VG <vikas11187@gmail.com>
2018-12-27 00:28:57 -0600

    + sae

--- core/NeuralLayers/__init__.py
+++ core/NeuralLayers/__init__.py
@@ -1,4 +1,16 @@
-""" TensorMONK's :: NeuralLayers                                             """
+""" TensorMONK's :: NeuralLayers                                            """
+
+__all__ = ["Linear", "Convolution", "ConvolutionTranspose",
+           "ResidualOriginal", "ResidualComplex", "ResidualInverted",
+           "ResidualShuffle", "ResidualNeXt", "SEResidualComplex",
+           "SEResidualNeXt", "SimpleFire", "CarryModular", "DenseBlock",
+           "Stem2", "InceptionA", "InceptionB", "InceptionC", "ReductionA",
+           "ReductionB", "ContextNet_Bottleneck",
+           "PrimaryCapsule", "RoutingCapsule",
+           "ConvolutionalSAE", "DetailPooling",
+           "CapsuleLoss", "CategoricalLoss", "TripletLoss", "DiceLoss",
+           "ObfuscateDecolor", "Activations", "Normalizations"]
+
 
 from .linear import Linear
 from .convolution import Convolution
@@ -6,13 +18,14 @@ from .convolutiontranspose import ConvolutionTranspose
 from .carryresidue import ResidualOriginal, ResidualComplex, \
     ResidualInverted, ResidualShuffle, ResidualNeXt, SEResidualComplex, \
     SEResidualNeXt, SimpleFire, CarryModular, DenseBlock, Stem2, InceptionA, \
-    InceptionB, InceptionC, ReductionA, ReductionB,ContextNet_Bottleneck
+    InceptionB, InceptionC, ReductionA, ReductionB, ContextNet_Bottleneck
 from .primarycapsule import PrimaryCapsule
 from .routingcapsule import RoutingCapsule
+from .sae import ConvolutionalSAE
 
 from .detailpooling import DetailPooling
 from .lossfunctions import CapsuleLoss, CategoricalLoss, TripletLoss, \
-    DiceLoss, CenterLoss
+    DiceLoss
 from .obfuscatedecolor import ObfuscateDecolor
 
 from .activations import Activations
@@ -29,3 +42,4 @@ del linear
 del detailpooling
 del lossfunctions
 del obfuscatedecolor
+del sae
