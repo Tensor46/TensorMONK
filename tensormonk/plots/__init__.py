764dd882dd710c4fd61fa99a17d9e5c8165cedf2
VG <vikas11187@gmail.com>
2019-01-16 19:16:53 -0600

    + line_plot

--- tensormonk/plots/__init__.py
+++ tensormonk/plots/__init__.py
@@ -1,8 +1,9 @@
 """ TensorMONK :: plots """
 
-__all__ = ["make_gif", "VisPlots"]
+__all__ = ["make_gif", "VisPlots", "line_plot"]
 
 from .gif import make_gif
 from .visplots import VisPlots
+from .line import line_plot
 
-del gif, visplots
+del gif, visplots, line
