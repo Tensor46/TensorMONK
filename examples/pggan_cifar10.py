688231bcb3559f4d53b6656549ba11b2348558cc
VG <vikas11187@gmail.com>
2019-01-16 19:48:26 -0600

    + fixed stability issues

--- examples/pggan_cifar10.py
+++ examples/pggan_cifar10.py
@@ -1,13 +1,16 @@
-""" TensorMONK's :: Progressing growth of GANs on CIFAR10                   """
+""" Training Progressing growth of GANs on CIFAR10 """
 
 from __future__ import print_function, division
 import os
 import sys
+import math
 import timeit
 import argparse
 import numpy as np
-import torch
 import tensormonk
+import torch.nn.functional as F
+
+MSG = "... {:6d} :: Cost df/dr/g {:2.3f}/{:2.3f}/{:2.3f} :: {:4d} I/S      "
 
 
 def parse_args():
@@ -20,18 +23,18 @@ def parse_args():
     parser.add_argument("--cpus", type=int,  default=4)
 
     parser.add_argument("--levels", type=int,  default=4)
-    parser.add_argument("--growth_rate", type=int,  default=32)
-    parser.add_argument("--n_embedding", type=int,  default=256)
+    parser.add_argument("--growth_rate", type=int,  default=64)
     parser.add_argument("--l1_iterations", type=int,  default=100000)
     parser.add_argument("-I", "--ignore_trained", action="store_true")
     return parser.parse_args()
 
 
-def noisy_latent(n, n_embedding):  # noise
-    return torch.randn(n, n_embedding)
-
-
 def train():
+    def cost_sampler(i):
+        return min(max(1, math.ceil(i / 1000.)), 1000)
+
+    sampler = 1
+
     args = parse_args()
     from tensormonk.data import DataSets
     trData, vaData, teData, n_labels, tensor_size = \
@@ -43,8 +46,7 @@ def train():
     file_name = "./models/pggan-cifar10"
     from tensormonk.plots import VisPlots, make_gif
     visplots = VisPlots(file_name.split("/")[-1].split(".")[0])
-    embedding_net_kwargs = {"n_embedding": args.n_embedding,
-                            "levels": args.levels, "l1_size": l1_size,
+    embedding_net_kwargs = {"levels": args.levels, "l1_size": l1_size,
                             "l1_iterations": args.l1_iterations,
                             "growth_rate": args.growth_rate}
     from tensormonk.essentials import MakeModel, SaveModel
@@ -54,13 +56,6 @@ def train():
                       default_gpu=args.default_gpu,
                       gpus=args.gpus,
                       ignore_trained=args.ignore_trained)
-
-    # optimizers
-    g_params, d_params = Model.netEmbedding.NET46.trainable_parameters()
-    g_optimizer = torch.optim.Adam(g_params, lr=args.learningRate,
-                                   amsgrad=True)
-    d_optimizer = torch.optim.Adam(d_params, lr=args.learningRate,
-                                   amsgrad=True)
 
     print(" ... level = {:d},".format(Model.netEmbedding.NET46.current_level) +
           " transition = {},".format("ON" if
@@ -82,33 +77,34 @@ def train():
             if Model.meterIterations >= Model.netEmbedding.NET46.total_ittr:
                 break
             Model.meterIterations += 1
+            Model.netEmbedding.NET46.iterations.add_(1)
+            Model.netEmbedding.NET46.update()
 
             # forward pass and parameter update
-            latent = noisy_latent(args.BSZ, args.n_embedding)
+            latent = Model.netEmbedding.NET46.noisy_latent(args.BSZ)
             fake = Model.netEmbedding(latent)
 
             # A constrain on the updates with respect to loss, so, nither wins!
             # train discriminator with real sample
             d_loss_real = Model.netEmbedding(tensor).mul(-1).add(1).mean()
-            d_optimizer.zero_grad()
-            if d_loss_real.data.cpu().numpy() > .1:
+            Model.netEmbedding.zero_grad()
+            if d_loss_real.data.cpu().numpy() >= .2:
                 d_loss_real.backward()
-                d_optimizer.step()
+                Model.netEmbedding.NET46.d_optimizer.step()
 
             # train discriminator with fake sample
             d_loss_fake = Model.netEmbedding(fake.detach()).mean()
-            d_optimizer.zero_grad()
-            if d_loss_fake.data.cpu().numpy() > .1:
+            Model.netEmbedding.zero_grad()
+            if d_loss_fake.data.cpu().numpy() >= .2:
                 d_loss_fake.backward()
-                d_optimizer.step()
-            d_loss = (d_loss_fake + d_loss_real) / 2
+                Model.netEmbedding.NET46.d_optimizer.step()
 
             # generate a fake sample
             g_loss = Model.netEmbedding(fake).mul(-1).add(1).mean()
-            g_optimizer.zero_grad()
-            if g_loss.data.cpu().numpy() > .1:
+            Model.netEmbedding.zero_grad()
+            if g_loss.data.cpu().numpy() >= .1:
                 g_loss.backward()
-                g_optimizer.step()
+                Model.netEmbedding.NET46.g_optimizer.step()
 
             # Visdom visualization + gifs
             if Model.meterIterations % 20 == 0:
@@ -116,46 +112,61 @@ def train():
                 if png_count == 20:
                     list_images = [os.path.join("./models", x) for x in
                                    next(os.walk("./models"))[2] if level_id
-                                   in x and x.endswith(".png")]
+                                   in x and x.endswith(".png") and
+                                   file_name.split("/")[-1] in x]
                     make_gif(list_images, file_name + "-" + level_id + ".gif")
                     png_count = 0
-                visplots.show_weights(Model.netEmbedding.state_dict())
                 name = "level" + level_id
+                fake = F.interpolate(fake if fake.size(0) < 64 else fake[:64],
+                                     size=tensor_size[2:])
                 visplots.show_images(fake.data.cpu(), name, file_name + "-" +
                                      name + "_" + str(png_count)+".png")
                 png_count += 1
-                visplots.show_images(tensor.data.cpu(), "real")
-            Model.netEmbedding.regularize_weights()
 
             # updating all meters
             Model.meterLoss.append(float(g_loss.cpu().data.numpy()))
-            Model.meterLoss.append(float(d_loss.cpu().data.numpy()))
+            Model.meterLoss.append(float(d_loss_real.cpu().data.numpy()))
+            Model.meterLoss.append(float(d_loss_fake.cpu().data.numpy()))
             Model.meterSpeed.append(int(float(args.BSZ) /
                                         (timeit.default_timer()-Timer)))
             Timer = timeit.default_timer()
-            print("... {:6d} :: Cost d/g {:2.3f}/{:2.3f} :: {:4d} I/S ".format(
-                Model.meterIterations, Model.meterLoss[-1],
-                Model.meterLoss[-2], Model.meterSpeed[-1]), end="\r")
+            print(MSG.format(Model.meterIterations,
+                             Model.meterLoss[-1],
+                             Model.meterLoss[-2],
+                             Model.meterLoss[-3],
+                             Model.meterSpeed[-1]), end="\r")
             sys.stdout.flush()
 
             # save and track
             if Model.meterIterations % 2000 == 0:
-                if np.mean(Model.meterLoss[1::2][-50:]) > 0.1 and \
-                   np.mean(Model.meterLoss[1::2][-50:]) < 0.9 and \
-                   np.mean(Model.meterLoss[0::2][-50:]) > 0.1 and \
-                   np.mean(Model.meterLoss[0::2][-50:]) < 0.9:
-                    # only save when last few g_loss and d_loss are in sensible
+                if not np.sum(np.isnan(Model.meterLoss[-50:])):
                     SaveModel(Model)
-                print("... {:6d} :: Cost d/g {:2.3f}/{:2.3f} :: ".format(
-                    Model.meterIterations,
-                    np.mean(Model.meterLoss[1::2][-1000:]),
-                    np.mean(Model.meterLoss[0::2][-1000:])) +
-                    "{:4d} I/S  ".format(
-                    int(np.mean(Model.meterSpeed[-2000:]))))
+                print(MSG.format(Model.meterIterations,
+                                 np.mean(Model.meterLoss[2::3][-1000:]),
+                                 np.mean(Model.meterLoss[1::3][-1000:]),
+                                 np.mean(Model.meterLoss[0::3][-1000:]),
+                                 int(np.mean(Model.meterSpeed[-2000:]))))
                 print(" ... level= {:d}, transition= {}, alpha= {:1.4}".format(
                     Model.netEmbedding.NET46.current_level, "ON" if
                     Model.netEmbedding.NET46.transition else "OFF",
                     Model.netEmbedding.NET46.alpha))
+
+            if Model.meterIterations % sampler == 0:
+                # Cost plot
+                sampler = cost_sampler(Model.meterIterations)
+                cost_samples = Model.meterLoss[0::3][0::sampler]
+                xaxis = np.arange(0, len(cost_samples)*sampler, sampler)
+                visplots.visplots.line(Y=np.array(cost_samples), X=xaxis,
+                                       opts={"title": "gCost"}, update=None,
+                                       win="gCost")
+                cost_samples = Model.meterLoss[1::3][0::sampler]
+                visplots.visplots.line(Y=np.array(cost_samples), X=xaxis,
+                                       opts={"title": "dCost-real"},
+                                       update=None, win="dCost-real")
+                cost_samples = Model.meterLoss[2::3][0::sampler]
+                visplots.visplots.line(Y=np.array(cost_samples), X=xaxis,
+                                       opts={"title": "dCost-fake"},
+                                       update=None, win="dCost-fake")
     print("\nDone with training")
 
 
