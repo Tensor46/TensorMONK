517ed49d3cd0d15bd9919b009dd1ea2fb62d6840
VG <vikas11187@gmail.com>
2019-01-16 19:36:45 -0600

    + stability issues

--- tensormonk/normalizations/pixelwise.py
+++ tensormonk/normalizations/pixelwise.py
@@ -7,12 +7,12 @@ import torch
 
 class PixelWise(torch.nn.Module):
     r""" Implemented - https://arxiv.org/pdf/1710.10196.pdf """
-    def __init__(self, eps=1e-8):
+    def __init__(self, eps=1e-6):
         super(PixelWise, self).__init__()
         self.eps = eps
 
     def forward(self, tensor):
-        return tensor.div(tensor.pow(2).mean(1, True).pow(.5) + self.eps)
+        return tensor.div(tensor.pow(2).mean(1, True).pow(.5).add(self.eps))
 
     def __repr__(self):
         return "pixelwise"
