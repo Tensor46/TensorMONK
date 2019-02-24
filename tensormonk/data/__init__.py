9c338d36aaa07eb8fb22d42b2b54e471fdf71a5e
VG <vikas11187@gmail.com>
2019-02-23 21:56:56 -0600

    + updated transforms

--- tensormonk/data/__init__.py
+++ tensormonk/data/__init__.py
@@ -1,9 +1,13 @@
 """ TensorMONK :: data """
 
-__all__ = ["DataSets", "FewPerLabel", "FolderITTR"]
+__all__ = ["DataSets", "FewPerLabel", "FolderITTR",
+           "Flip", "ElasticSimilarity",
+           "RandomBlur", "RandomTransforms"]
 
 from .datasets import DataSets
 from .fewperlabel import FewPerLabel
 from .folderittr import FolderITTR
+from .transforms import Flip, ElasticSimilarity, RandomBlur, \
+    RandomTransforms
 
-del datasets, fewperlabel, folderittr
+del datasets, fewperlabel, folderittr, transforms
