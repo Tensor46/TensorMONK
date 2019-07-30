346e900c465b74c10008e3520f9f3b59ee429e45
VG <vikas11187@gmail.com>
2019-07-30 15:18:56 -0500

    + Checks

--- tensormonk/utils/__init__.py
+++ tensormonk/utils/__init__.py
@@ -1,7 +1,7 @@
 """ TensorMONK :: utils """
 
 __all__ = ["roc", "ImageNetNorm", "Measures", "compute_affine",
-           "PillowUtils", "ObjectUtils", "SSDUtils", "Kernels"]
+           "PillowUtils", "ObjectUtils", "SSDUtils", "Kernels", "Checks"]
 
 from .roc import roc
 from .imagenetnorm import ImageNetNorm
@@ -10,5 +10,7 @@ from .computeaffine import compute_affine
 from .pillow_utils import PillowUtils
 from .object_utils import ObjectUtils, SSDUtils
 from .kernels import Kernels
+from .checks import Checks
 
-del measures, imagenetnorm, computeaffine, pillow_utils, object_utils, kernels
+del (checks, measures, imagenetnorm, computeaffine, pillow_utils,
+     object_utils, kernels)
