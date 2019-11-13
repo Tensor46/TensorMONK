dd286542616d2914237a828253af710d256cc48e
VG <vikas11187@gmail.com>
2019-11-13 17:53:31 -0600

    + iou loss & balanced l1 loss

--- tensormonk/loss/__init__.py
+++ tensormonk/loss/__init__.py
@@ -1,12 +1,15 @@
 """ TensorMONK :: loss """
 
 __all__ = ["CapsuleLoss", "Categorical", "DiceLoss",
-           "MetricLoss", "MultiBoxLoss", "TripletLoss"]
+           "MetricLoss", "MultiBoxLoss", "TripletLoss",
+           "BalancedL1Loss", "IOULoss"]
 
 from .categorical import Categorical
 from .capsuleloss import CapsuleLoss
 from .metricloss import TripletLoss, MetricLoss
 from .multibox import MultiBoxLoss
 from .segloss import DiceLoss
+from .iou_loss import IOULoss
+from .balanced_l1loss import BalancedL1Loss
 
-del categorical, capsuleloss, metricloss, segloss, multibox
+del categorical, capsuleloss, metricloss, segloss, multibox, iou_loss
