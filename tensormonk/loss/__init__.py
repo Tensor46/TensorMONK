7287b6ad9c2d73ada396e527bb7648e34a3f3fd0
VG <vikas11187@gmail.com>
2019-11-20 18:53:38 -0600

    + BoxesLoss & LabelLoss

--- tensormonk/loss/__init__.py
+++ tensormonk/loss/__init__.py
@@ -2,7 +2,8 @@
 
 __all__ = ["CapsuleLoss", "Categorical", "DiceLoss",
            "MetricLoss", "MultiBoxLoss", "TripletLoss",
-           "BalancedL1Loss", "IOULoss"]
+           "BalancedL1Loss", "IOULoss",
+           "LabelLoss", "BoxesLoss"]
 
 from .categorical import Categorical
 from .capsuleloss import CapsuleLoss
@@ -11,5 +12,8 @@ from .multibox import MultiBoxLoss
 from .segloss import DiceLoss
 from .iou_loss import IOULoss
 from .balanced_l1loss import BalancedL1Loss
+from .label_loss import LabelLoss
+from .boxes_loss import BoxesLoss
 
-del categorical, capsuleloss, metricloss, segloss, multibox, iou_loss
+del (categorical, capsuleloss, metricloss, segloss, multibox, iou_loss,
+     balanced_l1loss, label_loss, boxes_loss)
