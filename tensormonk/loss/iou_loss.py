d716a68324f2d30de21b711e3538bf2eee839d32
VG <vikas11187@gmail.com>
2019-11-21 20:47:38 -0600

    + -inf bug fix

--- tensormonk/loss/iou_loss.py
+++ tensormonk/loss/iou_loss.py
@@ -29,7 +29,7 @@ class IOULoss(nn.Module):
             options = None | "mean" | "sum"
             default = None
 
-        box_form (str): The format of predicted and targets.
+        box_form (str): The format of p_boxes and t_boxes.
             options = "d_ltrb" | "cxcywh"
             default = "d_ltrb"
 
@@ -37,6 +37,7 @@ class IOULoss(nn.Module):
             pixel (i, j) and the left, top, right, and bottom boundaries.
             cxcywh = (cx, cy, w, h) corner form boxes
     """
+    METHODS = ("iou", "log_iou", "giou", "log_giou")
 
     def __init__(self,
                  method: str = "log_iou",
@@ -49,7 +50,7 @@ class IOULoss(nn.Module):
             raise TypeError("IOULoss: method must be str: "
                             "{}".format(type(method).__name__))
         method = method.lower()
-        if method not in ["iou", "log_iou", "giou", "log_giou"]:
+        if method not in IOULoss.METHODS:
             raise ValueError("IOULoss: method must be "
                              "iou/log_iou/giou/log_giou: "
                              "{}".format(method))
@@ -76,31 +77,31 @@ class IOULoss(nn.Module):
         self.tensor_size = 1,
 
     def forward(self,
-                predicted: torch.Tensor,
-                targets: torch.Tensor,
+                p_boxes: torch.Tensor,
+                t_boxes: torch.Tensor,
                 weights: torch.Tensor = None):
         r"""
-        predicted: Must have Nx4 shape (N can vary per batch).
+        p_boxes: Must have Nx4 shape (N can vary per batch).
             Each box must be in the d_ltrb form; d_ltrb = (dl, dt, dr, db)
             (dl, dt, dr, db) are the distances between the location
             pixel (i, j) and the left, top, right, and bottom boundaries.
             Must be valid boxes not all the possible boxes on an image.
-        targets: Same as predicted.
+        t_boxes: Same as p_boxes.
         weights: None or tensor of shape Nx1 or N.
         """
 
-        assert predicted.size(-1) == 4 and targets.size(-1) == 4, \
-            "IOULoss: predicted/targets last dimension length != 4"
-        predicted, targets = predicted.view(-1, 4), targets.view(-1, 4)
-        assert predicted.size(0) == targets.size(0), \
-            "IOULoss: predicted.size(0) ~= targets.size(0)"
+        assert p_boxes.size(-1) == 4 and t_boxes.size(-1) == 4, \
+            "IOULoss: p_boxes/t_boxes last dimension length != 4"
+        p_boxes, t_boxes = p_boxes.view(-1, 4), t_boxes.view(-1, 4)
+        assert p_boxes.size(0) == t_boxes.size(0), \
+            "IOULoss: p_boxes.size(0) ~= t_boxes.size(0)"
 
         if self.is_offsets:
-            p_dl, p_dt, p_dr, p_db = [predicted[:, i] for i in range(4)]
-            t_dl, t_dt, t_dr, t_db = [targets[:, i] for i in range(4)]
+            p_dl, p_dt, p_dr, p_db = [p_boxes[:, i] for i in range(4)]
+            t_dl, t_dt, t_dr, t_db = [t_boxes[:, i] for i in range(4)]
             assert not (p_dl.lt(0).any() or p_dt.lt(0).any() or
                         p_dr.lt(0).any() or p_db.lt(0).any()), \
-                "IOULoss: predicted boxes offset must be >= 0 (use relu)"
+                "IOULoss: p_boxes boxes offset must be >= 0 (use relu)"
             assert not (t_dl.lt(0).any() or t_dt.lt(0).any() or
                         t_dr.lt(0).any() or t_db.lt(0).any()), \
                 "IOULoss: target boxes offset must be >= 0"
@@ -117,10 +118,10 @@ class IOULoss(nn.Module):
                       (torch.max(p_dt, t_dt) + torch.max(p_db, t_db)))
 
         else:
-            p_cx, p_cy, p_w, p_h = [predicted[:, i] for i in range(4)]
-            t_cx, t_cy, t_w, t_h = [targets[:, i] for i in range(4)]
+            p_cx, p_cy, p_w, p_h = [p_boxes[:, i] for i in range(4)]
+            t_cx, t_cy, t_w, t_h = [t_boxes[:, i] for i in range(4)]
             assert not (p_w.le(0).any() or p_h.le(0).any()), \
-                "IOULoss: predicted boxes are not in center form (use relu)"
+                "IOULoss: p_boxes boxes are not in center form (use relu)"
             assert not (t_w.le(0).any() or t_h.le(0).any()), \
                 "IOULoss: target boxes are not in center form"
             # intersection
@@ -141,7 +142,7 @@ class IOULoss(nn.Module):
                        torch.min(p_cy - p_h, t_cy - t_h)))
 
         # iou or generalized iou computation
-        ious = intersection / union
+        ious = intersection / union.add(1e-8)
         if self.method in ["giou", "log_giou"]:
             ious = ious - ((ac - union) / ac.add(1e-8))
 
@@ -149,7 +150,7 @@ class IOULoss(nn.Module):
         if self.method in ["iou", "giou"]:
             iou_loss = 1 - ious
         else:
-            iou_loss = - torch.log(ious)
+            iou_loss = - torch.log(ious.clamp(1e-8, 1))
 
         # weighted loss
         if weights is not None:
@@ -168,9 +169,9 @@ class IOULoss(nn.Module):
                                                       self.reduction)
 
 
-# predicted = torch.Tensor([[0.1, 0.2, 0.1, 0.3]])
-# targets = torch.Tensor([[0.2, 0.1, 0.2, 0.4]])
-# IOULoss("log_iou")(predicted, targets)
-# IOULoss("iou")(predicted, targets)
-# IOULoss("log_giou")(predicted, targets)
-# IOULoss("giou")(predicted, targets)
+# p_boxes = torch.Tensor([[0.1, 0.2, 0.1, 0.3]])
+# t_boxes = torch.Tensor([[0.2, 0.1, 0.2, 0.4]])
+# IOULoss("log_iou")(p_boxes, t_boxes)
+# IOULoss("iou")(p_boxes, t_boxes)
+# IOULoss("log_giou")(p_boxes, t_boxes)
+# IOULoss("giou")(p_boxes, t_boxes)
