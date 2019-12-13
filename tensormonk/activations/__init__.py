4a4175fefb5d2957dbbb2e9359151f46e0d5958e
VG <vikas11187@gmail.com>
2019-12-12 19:36:51 -0600

    - swish import

--- tensormonk/activations/__init__.py
+++ tensormonk/activations/__init__.py
@@ -1,7 +1,7 @@
 """ TensorMONK :: Activations """
 
-__all__ = ["Activations", "maxout", "squash", "swish"]
+__all__ = ["Activations", "maxout", "squash"]
 
-from .activations import Activations, maxout, squash, swish
+from .activations import Activations, maxout, squash
 
 del activations
