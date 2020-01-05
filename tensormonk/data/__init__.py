e0cb41291b7c4d453fb7663218ea3e4720b79ef5
VG <vikas11187@gmail.com>
2020-01-05 13:11:08 -0600

    + SuperResolutionData

--- tensormonk/data/__init__.py
+++ tensormonk/data/__init__.py
@@ -2,7 +2,8 @@
 
 __all__ = ["DataSets", "PascalVOC", "FewPerLabel", "FolderITTR",
            "Flip", "ElasticSimilarity", "LMDB",
-           "RandomBlur", "RandomColor", "RandomNoise", "RandomTransforms"]
+           "RandomBlur", "RandomColor", "RandomNoise", "RandomTransforms",
+           "SuperResolutionData"]
 
 from .datasets import DataSets
 from .pascalvoc import PascalVOC
@@ -11,5 +12,6 @@ from .folderittr import FolderITTR
 from .transforms import Flip, ElasticSimilarity, RandomBlur, RandomColor,\
     RandomNoise, RandomTransforms
 from .lmdb_db import LMDB
+from .sr_data import SuperResolutionData
 
 del datasets, fewperlabel, folderittr, transforms, pascalvoc, lmdb_db
