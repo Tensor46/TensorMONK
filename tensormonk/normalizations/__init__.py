3500e31f8e2431068c571a47abe6550b4f1beadc
VG <vikas11187@gmail.com>
2021-02-10 21:16:54 -0600

    + NormAbsMax

--- tensormonk/normalizations/__init__.py
+++ tensormonk/normalizations/__init__.py
@@ -1,5 +1,6 @@
 """ TensorMONK :: Normalizations """
 
-__all__ = ["Normalizations", "FrozenBatch2D"]
+__all__ = ["Normalizations", "FrozenBatch2D", "NormAbsMax"]
 
 from .normalizations import Normalizations, FrozenBatch2D
+from .norm_absmax import NormAbsMax
