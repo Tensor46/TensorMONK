48213f16e349f20b749f5409550ec72c3e9daa95
VG <vikas11187@gmail.com>
2021-02-11 20:55:49 -0600

    NormAbsMax to NormAbsMaxDynamic

--- tensormonk/normalizations/__init__.py
+++ tensormonk/normalizations/__init__.py
@@ -1,6 +1,7 @@
 """ TensorMONK :: Normalizations """
 
-__all__ = ["Normalizations", "FrozenBatch2D", "NormAbsMax", "NormAbsMax2d"]
+__all__ = ["Normalizations", "FrozenBatch2D",
+           "NormAbsMaxDynamic", "NormAbsMax2d"]
 
 from .normalizations import Normalizations, FrozenBatch2D
-from .norm_absmax import NormAbsMax, NormAbsMax2d
+from .norm_absmax import NormAbsMaxDynamic, NormAbsMax2d
